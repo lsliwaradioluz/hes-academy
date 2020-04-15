0feca8ce5b7d4c31422d8a1f276f3ae8487300e9
lsliwaradioluz <39009379+lsliwaradioluz@users.noreply.github.com>
2020-04-15 10:13:45 +0200

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <p>{{ programs }}</p>
+  </div>
+</template>
+
+<script>
+
+  import getAllPrograms from '~/apollo/queries/getAllPrograms.gql';
+
+  export default {
+    data() {
+      return {
+        programs: []
+      }
+    },
+    apollo: {
+      programs: {
+        query: getAllPrograms
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
