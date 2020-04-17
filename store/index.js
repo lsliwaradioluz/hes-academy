b3e3d0e403d23190330a2ca7bddaac69501a4833
lsliwaradioluz <39009379+lsliwaradioluz@users.noreply.github.com>
2020-04-17 10:52:53 +0200

    fix build error

--- store/index.js
+++ store/index.js
@@ -2,18 +2,18 @@ import cookieparser from 'cookieparser';
 
 export const actions = {  
   nuxtServerInit({ commit }, { req }) {
-    let user = null;
-    let cart = null;
-    let cartBackup = null;
-    if (req && req.headers && req.headers.cookie) {
-      const parsed = cookieparser.parse(req.headers.cookie);
-      user = (parsed.user && JSON.parse(parsed.user)) || null;
-      cart = (parsed.cart && JSON.parse(parsed.cart)) || null;
-      cartBackup = (parsed.cartBackup && JSON.parse(parsed.cartBackup)) || null;
-    }
+    // let user = null;
+    // let cart = null;
+    // let cartBackup = null;
+    // if (req && req.headers && req.headers.cookie) {
+    //   const parsed = cookieparser.parse(req.headers.cookie);
+    //   user = (parsed.user && JSON.parse(parsed.user)) || null;
+    //   cart = (parsed.cart && JSON.parse(parsed.cart)) || null;
+    //   cartBackup = (parsed.cartBackup && JSON.parse(parsed.cartBackup)) || null;
+    // }
     
-    commit('auth/setUser', user);
-    commit('cart/setItems', cart);
-    commit('cart/setCartBackup', cartBackup);
+    // commit('auth/setUser', user);
+    // commit('cart/setItems', cart);
+    // commit('cart/setCartBackup', cartBackup);
   }
 }
