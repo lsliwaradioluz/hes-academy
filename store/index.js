0b236418676f208351561b16115e6a4951d28e54
lsliwaradioluz <39009379+lsliwaradioluz@users.noreply.github.com>
2020-04-17 14:58:46 +0200

    fix build error

--- store/index.js
+++ store/index.js
@@ -1,37 +1,19 @@
 import cookieparser from 'cookieparser';
 
-export const state = () => ({
-  dupa: null
-});
-
-export const mutations = {
-  setDupa(state, payload) {
-    console.log('dupa was set');
-    state.dupa = payload;
-  }
-}
-
 export const actions = {  
   nuxtServerInit({ commit }, { req }) {
-    commit('setDupa', 'zbita');
-    // let user = null;
+    let user = null;
     // let cart = null;
     // let cartBackup = null;
-    // if (req && req.headers && req.headers.cookie) {
-    //   const parsed = cookieparser.parse(req.headers.cookie);
-    //   user = (parsed.user && JSON.parse(parsed.user)) || null;
-    //   cart = (parsed.cart && JSON.parse(parsed.cart)) || null;
-    //   cartBackup = (parsed.cartBackup && JSON.parse(parsed.cartBackup)) || null;
-    // }
+    if (req && req.headers && req.headers.cookie) {
+      const parsed = cookieparser.parse(req.headers.cookie);
+      user = (parsed.user && JSON.parse(parsed.user)) || null;
+      // cart = (parsed.cart && JSON.parse(parsed.cart)) || null;
+      // cartBackup = (parsed.cartBackup && JSON.parse(parsed.cartBackup)) || null;
+    }
     
-    // commit('auth/setUser', user);
+    commit('auth/setUser', user);
     // commit('cart/setItems', cart);
     // commit('cart/setCartBackup', cartBackup);
-  }
-}
-
-export const getters = {
-  dupa(state) {
-    return state.dupa;
   }
 }
