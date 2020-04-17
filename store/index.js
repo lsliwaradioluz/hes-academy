6b27c98281cc16cb6a495f6877670e2f440e89b8
lsliwaradioluz <39009379+lsliwaradioluz@users.noreply.github.com>
2020-04-17 11:03:50 +0200

    fix build error

--- store/index.js
+++ store/index.js
@@ -3,8 +3,8 @@ import cookieparser from 'cookieparser';
 export const actions = {  
   nuxtServerInit({ commit }, { req }) {
     let user = null;
-    let cart = [];
-    let cartBackup = [];
+    let cart = null;
+    let cartBackup = null;
     if (req && req.headers && req.headers.cookie) {
       const parsed = cookieparser.parse(req.headers.cookie);
       user = (parsed.user && JSON.parse(parsed.user)) || null;
